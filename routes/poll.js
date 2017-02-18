9acedee8d9899bf097746017e6c788ae54ba8fda
sophanarith sok <sophanarithsok@gmail.com>
2017-02-17 17:00:08 -0800

    improve add additional voting functionality

--- routes/poll.js
+++ routes/poll.js
@@ -5,6 +5,9 @@ const express = require('express'),
   User        = require('../models/users'),
   Poll        = require('../models/polls');
   
+// NOTE: Poll.findByIdAndUpdate methods in this file need major refactoring.
+// Consists of many DRY code.
+  
 // Find poll id via passed in req.parameter.id and send the JSON data as a 
 // response.
 router.get('/poll/:id', (req, res) => {
@@ -23,49 +26,65 @@ router.get('/poll/:id', (req, res) => {
 // When user casts a vote on a poll.
 router.post('/poll/:id', (req, res) => {
   // If user is signed in, allow user to cast a vote.
+  console.log(req.body);
   if(req.user) {
     
     // Store form sumbission vote value and username.
     let userVote       = req.body.vote,
       userName         = res.locals.user.username,
-      additionalOption = res.body.addOption;
+      additionalOption = req.body.addOption;
     
     // If additionalOption exists, add the new option and make it count as a
     // vote. Else, count the chosen radio button value.
     // NOTE: Make sure that a radio button and a provided additional option 
     // do not go through the form.
     if(additionalOption) {
+      // Find poll by ID and push in object with voter username and the 
+      // option that they voted for.  
+      Poll.findByIdAndUpdate({_id: req.params.id}, 
+        { $push: { voters: { user: userName, option: additionalOption }}},
+        { safe: true, upsert: true, new : true },
+        (err, res) => {
+          if(err) throw err;
+          console.log('Poll updated.');
+      });
       
+      // Find poll by ID and push in new object with new additional option 
+      // and vote count set to 1.
+      Poll.findByIdAndUpdate({_id: req.params.id}, 
+        { $push: { options: { option: additionalOption, votes: 1 }}},
+        { safe: true, upsert: true, new : true },
+        (err, res) => {
+          if(err) throw err;
+          console.log('Poll updated.');
+      });
     } else {
+      // Find poll by ID and push in object with voter username and the 
+      // option that they voted for.  
+      Poll.findByIdAndUpdate({_id: req.params.id}, 
+        { $push: { voters: { user: userName, option: userVote }}},
+        { safe: true, upsert: true, new : true },
+        (err, res) => {
+          if(err) throw err;
+          console.log('Poll updated.');
+      });
       
+      // Retrieve poll by option name and increment the vote key value pair 
+      // by one.
+      Poll.update({'options.option': userVote}, 
+        {$inc: {'options.$.votes': 1}}, (err, res) => {
+          if(err) throw err;
+          console.log('Votes incremented.');
+        }
+      );
+      res.render('viewPoll');
     }
     
-    // Find poll by ID and push in object with voter username and the 
-    // option that they voted for.  
-    Poll.findByIdAndUpdate({_id: req.params.id}, 
-      { $push: { voters: { user: userName, option: userVote }}},
-      { safe: true, upsert: true, new : true },
-      (err, res) => {
-        if(err) throw err;
-        console.log('Poll updated.');
-    });
-    
-    // Retrieve poll by option name and increment the vote key value pair 
-    // by one.
-    Poll.update({'options.option': userVote}, 
-      {$inc: {'options.$.votes': 1}}, (err, res) => {
-        if(err) throw err;
-        console.log('Votes incremented.');
-      }
-    );
-    res.render('viewPoll');
   } else {
     console.log('User is not logged in.');
     
     res.render('viewPoll');
   }
-  
-  console.log(req.body.addOption);
 });
   
 // When user directs to '/createpoll', render register.handlebars.
